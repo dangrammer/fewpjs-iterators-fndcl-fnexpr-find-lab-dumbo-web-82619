cde1cff23990905eea5d34ceb5288eed5e711d1e
dangrammer <danieljromans@gmail.com>
2019-12-26 20:53:18 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const testVar = {}
-
-function testFunc() {
-  return "hi"
+const superbowlWin = (data) => {
+  let result = data.find(obj => obj.result === 'W')
+  return result ? result.year : undefined
 }
